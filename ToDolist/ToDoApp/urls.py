e07e8e29e7b904a3aebdbaa9b6a92273a163a48b
arseni_Ub <novakarsenya@gmail.com>
2025-04-02 18:49:23 +0300

    change class HomePage View. HomePage is subclass FormMixin

--- ToDolist/ToDoApp/urls.py
+++ ToDolist/ToDoApp/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
-    path('newTask/', views.CreateTask.as_view(), name='create_task'),
     path('updateTask/<int:task_pk>/', views.UpdateTask.as_view(), name='update_task'),
     path('deleteTask/<int:task_pk>/', views.DeleteTask.as_view(), name='delete_task'),
+    path('futureTasks/', views.FuturePage.as_view(), name='future_tasks'),
+    path('archiveTasks/', views.ArchvePage.as_view(), name='archive_tasks'),
+    path('runningTask/<int:task_pk>/', views.RunningTask.as_view(), name='running_task'),
 ]
--- ToDolist/ToDoApp/views.py
+++ ToDolist/ToDoApp/views.py
@@ -1,48 +1,88 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, FormView, CreateView, UpdateView, DeleteView
+from django.views import View
+from django.views.generic import ListView, UpdateView, DeleteView, FormView
+from django.utils.timezone import now
+from django.views.generic.edit import FormMixin
 
 from .forms import AddTaskForm
 from .models import Task
 
 
-class HomePage(ListView):
+class HomePage(FormMixin,ListView):
     template_name = 'ToDoApp/index.html'
+    form_class = AddTaskForm
     model = Task
     context_object_name = 'tasks'
-    #form_class = AddTaskForm
-    #success_url = reverse_lazy('home')
+    success_url = reverse_lazy('home')
     extra_context = {
             'form': AddTaskForm,
+            'text_article': 'На сегодня больше нет задач, ты молодец',
+            'title_article': 'Задачи на сегодня',
     }
 
     def post(self, request, *args, **kwargs):
-        time_running = [request.POST['time_running_year'],
-                        request.POST['time_running_month'],
-                        request.POST['time_running_day'],
-                        ]
-        time_running = '-'.join(time_running)
-        new_task = Task(description=request.POST['description'],
-                        importance=request.POST['importance'],
-                        time_running=time_running
+        """
+        Handle POST requests: instantiate a form instance with the passed
+        POST variables and then check if it's valid.
+        """
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        new_task = Task(description=form.cleaned_data['description'],
+                        importance=form.cleaned_data['importance'],
+                        time_running=form.cleaned_data['time_running']
                         )
         new_task.save()
-        return HttpResponseRedirect(reverse_lazy('home'))
+
+        return HttpResponseRedirect(self.get_success_url())
 
 
+    def get_queryset(self):
+        return Task.objects.filter(time_running=now(), StatusTask=0)
 
-    # def get_queryset(self):
-    #     return Task.objects.filter(is_published=True)
 
-class CreateTask(CreateView):
-    template_name = 'ToDoApp/addTask.html'
-    form_class = AddTaskForm
+class FuturePage(ListView):
+    template_name = 'ToDoApp/list_task.html'
+    model = Task
+    context_object_name = 'tasks'
     extra_context = {
-        'title': 'Добавление задачи',
-        'ButtonSubbmit': 'Добавить задачу'
+        'text_article': 'Будущее у тебя пока без заботное',
+        'title_article': 'Задачи на будущее',
     }
+
+    def get_queryset(self):
+        return Task.objects.filter(time_running__gt=now())
+
+
+class ArchvePage(ListView):
+    template_name = 'ToDoApp/list_task.html'
+    model = Task
+    context_object_name = 'tasks'
+    extra_context = {
+        'text_article': 'ты пока ничего не сделал',
+        'title_article': 'Выполненые задачи',
+    }
+
+    def get_queryset(self):
+        return Task.objects.filter(time_running__lt=now())
+
+
+class DeleteTask(DeleteView):
+    template_name = 'ToDoApp/index.html'
+    model = Task
+    pk_url_kwarg = 'task_pk'
     success_url = reverse_lazy('home')
+
+    def get(self, request, *args, **kwargs):
+        del_task = Task.objects.get(pk=kwargs['task_pk'])
+        del_task.delete()
+        return HttpResponseRedirect(reverse_lazy('home'))
 
 class UpdateTask(UpdateView):
     template_name = 'ToDoApp/updateTask.html'
@@ -55,23 +95,11 @@ class UpdateTask(UpdateView):
     }
     success_url = reverse_lazy('home')
 
-class DeleteTask(DeleteView):
-    template_name = 'ToDoApp/index.html'
-    model = Task
-    pk_url_kwarg = 'task_pk'
-    success_url = reverse_lazy('home')
 
-    # def get(self, request, *args, **kwargs):
-    #     """Handle GET requests: instantiate a blank version of the form."""
-    #     return self.render_to_response(self.get_context_data())
-    #
-    # def post(self, request, *args, **kwargs):
-    #     """
-    #     Handle POST requests: instantiate a form instance with the passed
-    #     POST variables and then check if it's valid.
-    #     """
-    #     form = self.get_form()
-    #     if form.is_valid():
-    #         return self.form_valid(form)
-    #     else:
-    #         return self.form_invalid(form)
+class RunningTask(View):
+
+    def get(self, request, *args, **kwargs):
+        update_task = Task.objects.get(pk=kwargs['task_pk'])
+        update_task.StatusTask = 1
+        update_task.save()
+        return HttpResponseRedirect(reverse_lazy('home'))
